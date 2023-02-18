81de0c5f3b1f29c0f94d78173ba0883ffc00bd95
Kirtishrw <kirtishrw@gmail.com>
2023-02-19 00:17:08 +0530

    Initial commit

---
+++ MvcCoreLab/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreLab.Models;
+
+namespace MvcCoreLab.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Location"] = "India";
+            return View();
+        }
+
+        public IActionResult Student()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Kirti",
+                Gender = "Male"
+            };
+
+            ViewBag.CurrentDate = DateTime.Now.ToShortDateString();
+            ViewData["Title"] = "Dr.";
+            string? location = Convert.ToString(TempData["Location"]);
+            
+            return View(student);
+        }
+    }
+}
